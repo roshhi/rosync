5c4c7132443326a6f98ba136371356458ab46e49
MUHAMMAD-AHMAD-07 <not.roshaan@gmail.com>
2026-02-02 19:34:23 +0500

    refactor: update API service files for auth, file, folder, and share modules

--- frontend/src/services/authAPI.js
+++ frontend/src/services/authAPI.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:3000/auth';
+const API_URL = 'https://rosync.onrender.com/auth';
 
 export const authAPI = {
   signup: async (userData) => {
